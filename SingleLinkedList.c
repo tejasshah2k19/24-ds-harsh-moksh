521556587af9a4f063e243ba62e33bec28370a36
Tejas Shah <tejasshah2k19@gmail.com>
2024-09-20 16:48:49 +0530

    algo

--- SingleLinkedList.c
+++ SingleLinkedList.c
@@ -38,9 +38,26 @@ void addNodeBeg(int num)
     {
         tmp = malloc(sizeof(struct node));
         tmp->data = num;
-        tmp->next = head; 
-        head = tmp ;
+        tmp->next = head;
+        head = tmp;
     }
+}
+
+void delBeg(){
+    // if(head != NULL){
+        
+    // }
+    struct node *tmp= head ; 
+    head = head->next;
+    free(tmp);
+}
+
+void delLast(){
+    struct node *p;
+    for(p=head;p->next != last;p=p->next);
+    p->next = NULL; 
+    free(last);
+    last = p; 
 }
 
 void display()
@@ -53,8 +70,55 @@ void display()
     }
 }
 
-void search(int key){
+void search(int key)
+{
+}
+void addNodeAny(int source, int data)
+{
+    struct node  *tmp, *p = head;
 
+    int flag = 0;
+    for (p = head; p != NULL; p = p->next)
+    {
+        if (p->data == source)
+        {
+            flag = 1;
+            printf("\n%d added",data);
+            tmp = malloc(sizeof(struct node));
+            tmp->data = data;  
+            tmp->next = p->next; 
+            p->next  = tmp; 
+            break;
+        }
+    }
+
+    if (flag == 0)
+    {
+        printf("\nInvalid Source => %d",source);
+    }
+}
+
+void delAny(int num){//10 
+    struct node *p,*q; 
+    if(head->data == num){
+        delBeg();
+    }else if(last->data == num){
+        delLast();
+    }else{
+        for(q=head;q!=NULL;q=q->next){
+            if(q->next->data == num){
+                break;
+            }
+        }
+
+        if(q == NULL){
+            printf("\nInvalid Key");
+        }else{
+            p = q->next;
+            q->next = p->next; 
+            free(p); 
+        }
+    }
 }
 
 int main()
@@ -63,12 +127,18 @@ int main()
     addNode(10);
     addNode(20);
     addNode(30);
-    
+
     display();
     addNodeBeg(15);
     display();
 
     search(25);
+
+    addNodeAny(20, 400);
+    addNodeAny(200, 4000);
+    
+    
+    display();
 
     return 0;
 }
