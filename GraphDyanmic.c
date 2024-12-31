dcb3c3797871f53f1f7a8940b2ea603d1a5617ba
Tejas Shah <tejasshah2k19@gmail.com>
2024-12-31 15:00:45 +0530

    all data

--- GraphDyanmic.c
+++ GraphDyanmic.c
@@ -5,16 +5,21 @@ struct edge
 {
     int src;
     int dest;
+    int cost;
 };
 
 int main()
 {
     int totalEdges;
+    int totalVertex;
     int i, j;
-    int src, dest;
-
+    int src, dest, cost;
+    int *visited;
     struct edge *e;
+    int init;
 
+    printf("\nHow many vertex you have?");
+    scanf("%d", &totalVertex);
     printf("\nHow many edges you want to store ? ");
     scanf("%d", &totalEdges); // 4
 
@@ -23,12 +28,14 @@ int main()
     for (i = 0; i < totalEdges * 2; i++)
     {
         printf("\nEnter source and dest");
-        scanf("%d%d", &src, &dest); // 1 2
+        scanf("%d%d", &src, &dest, &cost); // 1 2
         e[i].src = src;
         e[i].dest = dest;
+        e[i].cost = cost;
         i++; // 1
         e[i].src = dest;
         e[i].dest = src;
+        e[i].cost = cost;
     }
 
     // source ---> 60
@@ -42,6 +49,27 @@ int main()
         if (e[i].src == src)
         {
             printf(" %d", e[i].dest);
+        }
+    }
+
+    // sort
+
+    // printf("\nEnter initila vertext");
+    // scanf
+    // init = 0;
+
+    visited = calloc(totalVertex, sizeof(int));
+
+    for (i = 0; i < totalEdges * 2; i++)
+    {
+
+        if (isVisited(e[i].src) == 0)
+        {
+            printf(" %d", e[i].src); // 0
+            if (isVisited(e[i].dest) == 0)
+            {
+                // 1
+            }
         }
     }
 
